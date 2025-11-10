b0ac73ec26fe070b43ec29fa492835e43ab4c219
Olehpt <papinchakolehh@gmail.com>
2025-11-10 19:49:42 +0200

    added cartitems & accountseller controller. cartitems requires correct delete rule for related fields

---
+++ Contollers/CartItemsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DBapp.Data;
+using DBapp.Models;
+
+namespace DBapp.Contollers
+{
+    public class CartItemsController : Controller
+    {
+        private readonly DB1Context _context;
+
+        public CartItemsController(DB1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: CartItems
+        public async Task<IActionResult> Index()
+        {
+            var dB1Context = _context.CartItems.Include(c => c.Product);
+            return View(await dB1Context.ToListAsync());
+        }
+
+        // GET: CartItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItems
+                .Include(c => c.Product)
+                .FirstOrDefaultAsync(m => m.CartItemId == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: CartItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CartItemId,Quantity,ProductId,UserId")] CartItem cartItem)
+        {
+            var lastId = _context.CartItems
+                     .Select(u => (int?)u.UserId)
+                     .Max();
+            if (lastId == null) cartItem.CartItemId = 0;
+            else cartItem.CartItemId = lastId.Value + 1;
+            _context.Add(cartItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CartItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItems.FindAsync(id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", cartItem.ProductId);
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CartItemId,Quantity,ProductId,UserId")] CartItem cartItem)
+        {
+            if (id != cartItem.CartItemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cartItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartItemExists(cartItem.CartItemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", cartItem.ProductId);
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItems
+                .Include(c => c.Product)
+                .FirstOrDefaultAsync(m => m.CartItemId == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cartItem = await _context.CartItems.FindAsync(id);
+            if (cartItem != null)
+            {
+                _context.CartItems.Remove(cartItem);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartItemExists(int id)
+        {
+            return _context.CartItems.Any(e => e.CartItemId == id);
+        }
+    }
+}
---
+++ Contollers/SellerAccountsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DBapp.Data;
+using DBapp.Models;
+
+namespace DBapp.Contollers
+{
+    public class SellerAccountsController : Controller
+    {
+        private readonly DB1Context _context;
+
+        public SellerAccountsController(DB1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: SellerAccounts
+        public async Task<IActionResult> Index()
+        {
+            var dB1Context = _context.SellerAccounts.Include(s => s.Seller).Include(s => s.User);
+            return View(await dB1Context.ToListAsync());
+        }
+
+        // GET: SellerAccounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sellerAccount = await _context.SellerAccounts
+                .Include(s => s.Seller)
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.SellerAccountId == id);
+            if (sellerAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(sellerAccount);
+        }
+
+        // GET: SellerAccounts/Create
+        public IActionResult Create()
+        {
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: SellerAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SellerAccountId,UserId,SellerId")] SellerAccount sellerAccount)
+        {
+            var lastId = _context.SellerAccounts
+                     .Select(u => (int?)u.SellerAccountId)
+                     .Max();
+            if (lastId == null) sellerAccount.SellerAccountId = 0;
+            else sellerAccount.SellerAccountId = lastId.Value + 1;
+            _context.Add(sellerAccount);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: SellerAccounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sellerAccount = await _context.SellerAccounts.FindAsync(id);
+            if (sellerAccount == null)
+            {
+                return NotFound();
+            }
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId", sellerAccount.SellerId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", sellerAccount.UserId);
+            return View(sellerAccount);
+        }
+
+        // POST: SellerAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SellerAccountId,UserId,SellerId")] SellerAccount sellerAccount)
+        {
+            if (id != sellerAccount.SellerAccountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sellerAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SellerAccountExists(sellerAccount.SellerAccountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "SellerId", "SellerId", sellerAccount.SellerId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", sellerAccount.UserId);
+            return View(sellerAccount);
+        }
+
+        // GET: SellerAccounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sellerAccount = await _context.SellerAccounts
+                .Include(s => s.Seller)
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.SellerAccountId == id);
+            if (sellerAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(sellerAccount);
+        }
+
+        // POST: SellerAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sellerAccount = await _context.SellerAccounts.FindAsync(id);
+            if (sellerAccount != null)
+            {
+                _context.SellerAccounts.Remove(sellerAccount);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SellerAccountExists(int id)
+        {
+            return _context.SellerAccounts.Any(e => e.SellerAccountId == id);
+        }
+    }
+}
