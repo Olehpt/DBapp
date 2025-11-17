73596aa38c1d5bc5139638c7bfa0e091543c84a7
Olehpt <papinchakolehh@gmail.com>
2025-11-17 13:23:21 +0200

    added querries controller. Added 1 querry

---
+++ Contollers/QuerriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DBapp.Data;
+using DBapp.Models;
+namespace DBapp.Contollers
+{
+    public class QuerriesController : Controller
+    {
+        private readonly DB1Context _context;
+        public QuerriesController(DB1Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(double? targetprice)
+        {
+            if (targetprice != null)
+            {
+                ViewBag.Products = _context.Products
+                    .Where(p => p.UnitPrice < targetprice)
+                    .ToList();
+            }
+            return View();
+        }
+    }
+}
